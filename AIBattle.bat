cf68146392c4f4421415e71ebf56fd23df86ef56
woinck <hu_woinck@126.com>
2013-12-04 00:16:12 +0800

    Battle bat completed

--- AIBattle.bat
+++ AIBattle.bat
@@ -1,14 +1,13 @@
 @echo off 
 setlocal enabledelayedexpansion 
 
-dir>AIList.txt
+dir PlayerAI>AIList.txt
 
 for /f "skip=5 tokens=4,5 delims=. " %%i in (AIList.txt) do (
 	if %%j EQU exe (
 		for /f "skip=5 tokens=4,5 delims=. " %%k in (AIList.txt) do (
 			if %%l EQU exe (
-				echo %%i.%%j vs %%k.%%l
-				rem RUN GAME HERE!!!!!!!!!!!!
+				python sserver.py %%i.%%j vs %%k.%%l
 			)
 		)
 	)
