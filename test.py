9ebdfe61952f46d45e19debf5c7995196384f564
woinck <hu_woinck@126.com>
2013-12-11 23:54:32 +0800

    solve mirror bug

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import basic,sio
 
-map,base = sio._ReadFile("C:\Users\woinck\Documents\GitHub\DS15-dev\Maps\\4.map")
+replayInfo = sio._ReadFile("C:\Users\woinck\Desktop\Sample_vs_Sample_20131211-22-56-04.rep")
 
-for i in base:
-	for j in i:
-		print j.__dict__
+for i in range(30):
+	flag = False
+	try:
+		print 'i:',i
+		print 'move:',replayInfo[i][1].move
+		print 'order:',replayInfo[i][1].order
+		print 'target:',replayInfo[i][1].target
+		print '========================='
+	except:
+		pass
+	
+raw_input()
